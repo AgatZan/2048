119e7731505a9432d8f8712ebf50f7a473afeb7a
gordey <cornaple@gmail.com>
2024-07-17 02:36:02 +0300

    test-break

--- test/game.js
+++ test/game.js
@@ -4,5 +4,5 @@ const test = require("node:test");
 const assert = require("node:assert/strict");
 test("synchronous passing test", (t) => {
 	// This test passes because it does not throw an exception.
-	assert.strictEqual(1, 1);
+	assert.strictEqual(2, 1);
 });
