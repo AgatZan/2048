f7f334d7c8ab4a9c6446da74dd797a7cb7a18c85
gordey <cornaple@gmail.com>
2024-07-17 02:19:59 +0300

    settup proj

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+	{ languageOptions: { globals: globals.browser } },
+	{
+		ignores: ["**/node_modules/*.js", "**/test/*.js"],
+		rules: pluginJs.configs.recommended["rules"],
+	},
 ];
---
+++ test/game.js
@@ -0,0 +1,8 @@
+// import test from "node:test";
+// import { strict as assert } from "node:assert";
+const test = require("node:test");
+const assert = require("node:assert/strict");
+test("synchronous passing test", (t) => {
+	// This test passes because it does not throw an exception.
+	assert.strictEqual(1, 1);
+});
